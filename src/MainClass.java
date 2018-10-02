c1f50810f819b5fbe57abfbd22d67969643e108f
metelevamd <metelevamd@KM-DIT-N01.doronichi.com>
2018-10-02 09:28:18 +0300

    2

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,10 +1,56 @@
+import java.util.Scanner;
 
 public class MainClass {
 
 	public static void main(String args[])
 	{
 		
-		System.out.println();
+		boolean exit = false;
+		Scanner in = new Scanner(System.in);
+		
+		while (!exit) {
+			
+			System.out.println("> Размерность матрицы >> (4;8)");
+			System.out.println("> Выход >> 0");
+			System.out.print("\n  >> ");
+			
+			int size = 0;			
+			
+			try { 
+				size = in.nextInt(); 
+				
+				if (size == 0) exit = true;
+				else if (size < 4 || size > 8) throw new Exception();
+				
+			} catch (Exception e) { 
+				System.out.println("\n> Неверный ввод, пожалуйста, введите число (4;8)\n");
+				continue; 
+			}
+			
+			if (!exit) {			
+				System.out.println();
+				
+				System.out.println(">> МАТРИЦА СМЕЖНОСТИ >\n");
+//				boolean A[][] = Matrix.createBOOLEAN(size);
+				boolean A[][] = {{false, true, true, false, true, false}, 
+								 {false, true, false, false, true, false}, 
+								 {false, false, false, false, false, false}, 
+								 {false, false, true, false, false, false}, 
+								 {false, false, false, true, false, false}, 
+								 {true, false, false, false, true, true}};
+		        Matrix.printBOOLEAN(A, 'x', 'x'); 
+		        System.out.println("\n");
+		        
+		        boolean b = Matrix.isGraphBipartite(A);	
+		        if (b) System.out.println(">> ГРАФ ЯВЛЯЕТСЯ ДВУДОЛЬНЫМ");
+		        	else System.out.println(">> ГРАФ НЕ ЯВЛЯЕТСЯ ДВУДОЛЬНЫМ");		        
+		        
+		        System.out.println("\n\n");	
+		        
+			} 
+		}
+		
+		in.close();
 		
 	}
 	
--- src/Matrix.java
+++ src/Matrix.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
 import java.util.Random;
-
-// создание матрицы с рандомными элементами 
 
 public class Matrix {
 	
@@ -45,59 +42,10 @@ public class Matrix {
 		}		
 	}
 	
-	public static void printDescription(int A[][]) {	  	
+	//--------------------------------------------------------------//
+	public static boolean isGraphBipartite(boolean[][] A) {
 		
-		for (int i = 0; i < A.length; i ++)  
-			System.out.println("        a(" + i + ") = {x(" + A[i][0] + "), x(" + A[i][1] + ")}");
-		
-	}
-	
-	//---------------------------------------------------------//
-	public static boolean[][] getIncidenceMatrix(boolean A[][]) {
-		
-		int num = 0; // число дуг
-		for (int i = 0; i < A.length; i++)
-			for (int j = 0; j < A[0].length; j++) 
-				if (A[i][j]) num++;
-		
-		boolean B[][] = new boolean[A.length][num];
-		
-		int k = 0;
-		for (int i = 0; i < A.length; i++) 			
-			for (int j = 0; j < A[0].length; j++) 
-				if (A[i][j] && (i != j)) {
-					B[i][k] = true;
-					B[j][k] = true;
-					k++;
-				}
-
-		return B;
-		
-	}
-	
-	public static int[][] getDescription(boolean A[][]) { // A -- матрица инциденций
-		
-		int B[][] = new int[A[0].length][2];
-		
-		int k = 0;
-		boolean f = true;
-		
-		for (int i = 0; i < A[0].length; i++) 
-			for (int j = 0; j < A.length; j++) 
-				if (A[j][i]) {
-					
-					if (f) {
-						B[k][0] = j;						
-					} else {
-						B[k][1] = j;
-						k++;
-					}
-					
-					f = !f;					
-				}
-		
-		
-		return B;
+		return false;		
 		
 	}
 	
